8f44677737d88b4e9b419dfb63f23b10eaa451b4
ACGMN <ootopoo@vip.qq.com>
2026-01-23 00:10:22 +0800

    Init

---
+++ fractal-egui-demo_bg.wasm.d.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: (a: number, b: number) => number;
+export const wasm_bindgen__closure__destroy__h32d353c450b117cf: (a: number, b: number) => void;
+export const wasm_bindgen__closure__destroy__h8dd7dba0817a47c1: (a: number, b: number) => void;
+export const wasm_bindgen__convert__closures_____invoke__h71b2bca71125f70f: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h5798623e0f0e4e7b: (a: number, b: number) => [number, number];
+export const wasm_bindgen__convert__closures_____invoke__h75bd276d81efa83d: (a: number, b: number, c: any) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
